e7faf8d869a250e9a202171153f717cfc91b4eab
satyap54 <satyabratp.m.2000@gmail.com>
2024-02-23 14:54:20 +0530

    list vc as of now as home

--- CryptoMarket/SceneDelegate.swift
+++ CryptoMarket/SceneDelegate.swift
@@ -18,8 +18,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let viewController = UIViewController()
-        let navigationVC = UINavigationController(rootViewController: viewController)
+        
+        let vc = CryptoListCoordinator(dataSource: CryptoListDataSource(coreDataManager: CoreDataManager.shared)).createViewController()
+        
+        let navigationVC = UINavigationController(rootViewController: vc)
         window?.rootViewController = navigationVC
         window?.makeKeyAndVisible()
     }
